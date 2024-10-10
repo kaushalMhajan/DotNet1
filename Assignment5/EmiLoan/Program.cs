09dbc386eec22c7be71f24f9fcb8b3c40796b08c
Kaushal Atmaram Mahajan <142385433+kaushalMhajan@users.noreply.github.com>
2024-10-10 23:18:16 +0530

    Add files via upload

---
+++ Assignment5/EmiLoan/Program.cs
@@ -0,0 +1,12 @@
+﻿using EmiCalc;
+
+try{
+	Loan pl = Loaner.PersonalLoanScheme(220100,5);
+   	Console.WriteLine("your PersonalLoan emi is : {0:0.00} ",Loaner.GetEmI(pl));
+
+   	Loan hl = Loaner.HomeLoanScheme(220100,5);
+   	Console.WriteLine("your HomeLoan emi is : {0:0.00} ",Loaner.GetEmI(hl));
+}
+catch(Exception ex){
+	Console.WriteLine(ex.Message);
+}
