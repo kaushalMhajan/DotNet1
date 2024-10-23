ea29bb633f9c8be6f8a3180d54045f71ab9d7c78
Kaushal Atmaram Mahajan <142385433+kaushalMhajan@users.noreply.github.com>
2024-10-23 19:28:03 +0530

    Delete DBFirst directory

--- DBFirst/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Employment.Data;
-
-using var emp = new EmpDbContext();
-
-foreach(var employee in emp.Employees){
-    Console.WriteLine("{0} {1} {2}",employee.Id,employee.EmployeeName,employee.DepartmentId);
-}
-
